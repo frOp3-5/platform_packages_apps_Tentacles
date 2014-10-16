CarbonDev/android_packages_apps_CarbonFibers

CarbonFibers - Some fixes
Change-Id: I71b8d528b9e5d624e7ecd45a8529a4894bf939f8
Signed-off-by: STELIX <ssspinni@gmail.com>

src/com/carbon/fibers/fragments/MoreDeviceSettings.java
@@ -25,8 +25,9 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
-import com.carbon.fibers.R;
 import com.carbon.fibers.preference.SettingsPreferenceFragment;
+import com.carbon.fibers.R;
+import com.carbon.fibers.Utils;
 
 public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String TAG = "MoreDeviceSettings";
@@ -35,6 +36,7 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String KEY_DISPLAY_CALIBRATION_CATEGORY = "display_calibration_category";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
+    private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +44,9 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.more_device_settings);
         ContentResolver resolver = getContentResolver();
+
+        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
+                getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
 
         final PreferenceGroup sensorsCategory =
                 (PreferenceGroup) findPreference(KEY_SENSORS_MOTORS_CATEGORY);

frOp3-5/platform_packages_apps_Tentacles

Conflicts:
	src/com/oct/tentacles/fragments/MoreDeviceSettings.java

src/com/oct/tentacles/fragments/MoreDeviceSettings.java
@@ -25,8 +25,9 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
-import com.oct.tentacles.R;
 import com.oct.tentacles.preference.SettingsPreferenceFragment;
+import com.oct.tentacles.R;
+import com.oct.tentacles.Utils;
 
 public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String TAG = "MoreDeviceSettings";
@@ -35,6 +36,7 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String KEY_DISPLAY_CALIBRATION_CATEGORY = "display_calibration_category";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
+    private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +44,9 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.more_device_settings);
         ContentResolver resolver = getContentResolver();
+
+        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
+                getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
 
         final PreferenceGroup sensorsCategory =
                 (PreferenceGroup) findPreference(KEY_SENSORS_MOTORS_CATEGORY);