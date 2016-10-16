DirtyUnicorns/android_packages_apps_DU-Tweaks

Power/reboot dialogs dim [2/2]
- Controls the dim amount of the power/reboot menu dialog

Change-Id: Ia73f57730a139f1aabfc23f42379090317012d88

src/com/dirtyunicorns/dutweaks/fragments/PowerMenu.java
@@ -34,14 +34,17 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.util.du.DuUtils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.dirtyunicorns.dutweaks.preference.CustomSeekBarPreference;
 
 public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String POWER_REBOOT_DIALOG_DIM = "power_reboot_dialog_dim";
 
     private ListPreference mAdvancedReboot;
     private SwitchPreference mPowermenuTorch;
+    private CustomSeekBarPreference mPowerRebootDialogDim;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +69,12 @@ public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenc
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mPowerRebootDialogDim = (CustomSeekBarPreference) prefScreen.findPreference(POWER_REBOOT_DIALOG_DIM);
+        int powerRebootDialogDim = Settings.System.getInt(resolver,
+                Settings.System.POWER_REBOOT_DIALOG_DIM, 50);
+        mPowerRebootDialogDim.setValue(powerRebootDialogDim / 1);
+        mPowerRebootDialogDim.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -89,6 +98,11 @@ public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenc
             boolean checked = ((SwitchPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, checked ? 1:0);
+            return true;
+        } else if (preference == mPowerRebootDialogDim) {
+            int alpha = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_REBOOT_DIALOG_DIM, alpha * 1);
             return true;
         }
         return false;

frOp3-5/platform_packages_apps_Tentacles

Change-Id: I8680dfe7382873753567bb0ba8f68b7c0af48519

src/com/teamoctos/tentacles/fragments/PowerMenu.java
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD:src/com/teamoctos/tentacles/fragments/PowerMenu.java
  * Copyright (C) 2016 Team-OctOS
-=======
- * Copyright (C) 2014-2016 The Dirty Unicorns Project
->>>>>>> ee52854... Add MiscTweaks category:src/com/dirtyunicorns/dutweaks/fragments/PowerMenu.java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,14 +34,17 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.util.du.DuUtils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.teamoctos.tentacles.preference.CustomSeekBarPreference;
 
 public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String POWER_REBOOT_DIALOG_DIM = "power_reboot_dialog_dim";
 
     private ListPreference mAdvancedReboot;
     private SwitchPreference mPowermenuTorch;
+    private CustomSeekBarPreference mPowerRebootDialogDim;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,6 +69,12 @@ public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenc
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mPowerRebootDialogDim = (CustomSeekBarPreference) prefScreen.findPreference(POWER_REBOOT_DIALOG_DIM);
+        int powerRebootDialogDim = Settings.System.getInt(resolver,
+                Settings.System.POWER_REBOOT_DIALOG_DIM, 50);
+        mPowerRebootDialogDim.setValue(powerRebootDialogDim / 1);
+        mPowerRebootDialogDim.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -93,6 +98,11 @@ public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenc
             boolean checked = ((SwitchPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, checked ? 1:0);
+            return true;
+        } else if (preference == mPowerRebootDialogDim) {
+            int alpha = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_REBOOT_DIALOG_DIM, alpha * 1);
             return true;
         }
         return false;